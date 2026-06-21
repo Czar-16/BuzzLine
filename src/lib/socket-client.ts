fc25b221e7135e3c82a56bc4603e92299041dd43
Czar-16 <anoopjha2003@gmail.com>
2026-06-21 16:48:36 +0530

    Fix production socket connection (socket-client)

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -1,2 +1,7 @@
+// import { io } from "socket.io-client";
+// export const socket = io("http://localhost:3001");
+
+
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3001");
+
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!);
