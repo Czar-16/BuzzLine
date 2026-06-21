8747e72d843cfdde7c5ed6df849230e9ce4c065c
Czar-16 <anoopjha2003@gmail.com>
2026-06-21 16:57:38 +0530

    Add production CORS configuration

--- socket-server/server.js
+++ socket-server/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://buzzline-seven.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
